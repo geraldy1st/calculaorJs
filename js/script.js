3246bbd5e87803bac09ed0838fc9b59822fbed09
geraldy1st <geraldy.leondas@gmail.com>
2018-05-22 17:57:17 +0200

    css ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
+function one(){
+  document.getElementById('Ans').value +='1';
+}
+function two(){
+  document.getElementById('Ans').value +='2';
+}
+function three(){
+  document.getElementById('Ans').value +='3';
+}
+function plus(){
+  document.getElementById('Ans').value +='+';
+}
 
+function four(){
+  document.getElementById('Ans').value +='4';
+}
+function five(){
+  document.getElementById('Ans').value +='5';
+}
+function six(){
+  document.getElementById('Ans').value +='6';
+}
+function minus(){
+  document.getElementById('Ans').value +='-';
+}
+
+function seven(){
+  document.getElementById('Ans').value +='7';
+}
+function eight(){
+  document.getElementById('Ans').value +='8';
+}
+function nine(){
+  document.getElementById('Ans').value +='9';
+}
+function multiply(){
+  document.getElementById('Ans').value +='*';
+}
+
+function c(){
+  document.getElementById('Ans').value ='';
+}
+function zero(){
+  document.getElementById('Ans').value +='0';
+}
+function equal(){
+  document.getElementById('Ans').value = eval(document.getElementById('Ans').value);
+}
+function divide(){
+  document.getElementById('Ans').value +='/';
+}
